391de22863d6906993462bbdb8e0b64690d8ac97
Emma Skovgaard <emsko@danskebank.dk>
2026-06-17 15:30:15 +0200

    Added simple sample script

---
+++ nodejs/run_extract.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { sear } = require('./sear');
+
+try {
+    request = {
+        "operation": "extract",
+        "admin_type": "user",
+        "userid": "<userid>",
+    }
+    const response = sear(request, true);
+    console.log('\nResult:');
+    console.log(JSON.stringify(response.result, null, 2));
+} catch (error) {
+    console.error('SEAR extract failed:', error.message);
+    process.exit(1);
+}
