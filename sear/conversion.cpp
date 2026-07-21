3815815a0cad3c3c9c8afeb287d37eeabc08c371
Emma Skovgaard <emsko@danskebank.dk>
2026-07-21 17:08:22 +0200

    Fixed a potential memory leak with the encoding system

--- sear/conversion.cpp
+++ sear/conversion.cpp
@@ -1,50 +1,75 @@
 #include "conversion.hpp"
+
+#include <cerrno>
 #include <iconv.h>
 #include <stdexcept>
 #include <vector>
 
-std::string convert(std::string input, std::string_view inputCodepage, std::string_view outputCodepage) {
-    std::string fromCode{inputCodepage};
-    std::string toCode{outputCodepage};
-    iconv_t conv = ::iconv_open(toCode.c_str(), fromCode.c_str());
+namespace {
+class IconvHandle {
+ public:
+  IconvHandle(const char *to_code, const char *from_code)
+      : handle_(::iconv_open(to_code, from_code)) {}
 
-    if (conv == (iconv_t)-1) {
-      if (errno == EINVAL)
-        throw std::runtime_error(
-            "not supported from " + fromCode + " to " + toCode);
-      else
-        throw std::runtime_error("unknown error");
+  ~IconvHandle() {
+    if (handle_ != reinterpret_cast<iconv_t>(-1)) {
+      ::iconv_close(handle_);
     }
+  }
 
-    char* src_ptr = &input[0];
-    size_t src_size = input.size();
+  IconvHandle(const IconvHandle &)            = delete;
+  IconvHandle &operator=(const IconvHandle &) = delete;
 
-    std::vector<char> buf(1024);
-    std::string dst;
+  iconv_t get() const { return handle_; }
 
-    while (0 < src_size) {
-      char* dst_ptr = &buf[0];
-      size_t dst_size = buf.size();
-      size_t res = ::iconv(conv, &src_ptr, &src_size, &dst_ptr, &dst_size);
-      if (res == (size_t)-1) {
-        if (errno == E2BIG)  {
-          // ignore this error
-        } else {
-            switch (errno) {
-            case EILSEQ:
-            case EINVAL:
-                throw std::runtime_error("invalid multibyte chars");
-            default:
-                throw std::runtime_error("unknown error");
-            }
+  bool valid() const { return handle_ != reinterpret_cast<iconv_t>(-1); }
+
+ private:
+  iconv_t handle_;
+};
+}  // namespace
+
+std::string convert(std::string input, std::string_view inputCodepage,
+                    std::string_view outputCodepage) {
+  std::string fromCode{inputCodepage};
+  std::string toCode{outputCodepage};
+  IconvHandle conv(toCode.c_str(), fromCode.c_str());
+
+  if (!conv.valid()) {
+    if (errno == EINVAL)
+      throw std::runtime_error("not supported from " + fromCode + " to " +
+                               toCode);
+    else
+      throw std::runtime_error("unknown error");
+  }
+
+  char *src_ptr   = input.data();
+  size_t src_size = input.size();
+
+  std::vector<char> buf(1024);
+  std::string dst;
+
+  while (0 < src_size) {
+    char *dst_ptr   = buf.data();
+    size_t dst_size = buf.size();
+    size_t res = ::iconv(conv.get(), &src_ptr, &src_size, &dst_ptr, &dst_size);
+    if (res == static_cast<size_t>(-1)) {
+      if (errno == E2BIG) {
+        // ignore this error
+      } else {
+        switch (errno) {
+          case EILSEQ:
+          case EINVAL:
+            throw std::runtime_error("invalid multibyte chars");
+          default:
+            throw std::runtime_error("unknown error");
         }
       }
-      dst.append(&buf[0], buf.size() - dst_size);
     }
+    dst.append(buf.data(), buf.size() - dst_size);
+  }
 
-    iconv_close(conv);
-
-    return dst;
+  return dst;
 }
 
 /** Converts string from specified codepage to UTF-8, defaults to IBM-1047 if nothing is specified */
