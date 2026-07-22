1da06ae63f397e0c9ad7a2540982d9441c228b81
Emma Skovgaard <emsko@danskebank.dk>
2026-07-22 11:13:35 +0200

    Added more data validation

--- sear/irrseq00/profile_extractor.cpp
+++ sear/irrseq00/profile_extractor.cpp
@@ -122,7 +122,8 @@ void ProfileExtractor::extract(SecurityRequest &request) {
     // For search functions first try regular extract in case an existing name
     // was given as filter
     uint8_t save_function_code = function_code;
-    if (!request.getProfileName().empty() && request.getProfileName() != " ") {
+    if (!request.getProfileName().empty() && request.getProfileName() != " " &&
+        request.getProfileName()[0] != '\0') {
       switch (function_code) {
         case USER_EXTRACT_NEXT_FUNCTION_CODE:
           function_code                  = USER_EXTRACT_FUNCTION_CODE;
