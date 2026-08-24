55ec2e7dd35eeea9a3319c195c1c1cc2920004ff
Emma Skovgaard <emsko@danskebank.dk>
2026-08-24 12:40:29 +0200

    Fixed some bugs

--- sear/racroute/racroute_auth.s
+++ sear/racroute/racroute_auth.s
@@ -3,16 +3,36 @@
 *---------------------------------------------------------------------*
          SYSSTATE AMODE64=YES,ARCHLVL=OSREL,OSREL=SYSSTATE
 
+*---------------------------------------------------------------------*
+* Register equates.                                                   *
+*---------------------------------------------------------------------*
+R0       EQU   0
+R1       EQU   1
+R2       EQU   2
+R3       EQU   3
+R4       EQU   4
+R5       EQU   5
+R6       EQU   6
+R7       EQU   7
+R8       EQU   8
+R9       EQU   9
+R10      EQU   10
+R11      EQU   11
+R12      EQU   12
+R13      EQU   13
+R14      EQU   14
+R15      EQU   15
+
 RACFAUTH TITLE 'RACROUTE AUTHORIZATION'
 
 RACFAUTH CELQPRLG DSASIZE=DSASIZ,                                      X
                BASEREG=R12,                                            X
                PARMWRDS=5,                                             X
                PARMREG=R11,                                            X
-               ENTNAME=sear_racroute_auth_asm,                         X
+               ENTNAME=callRauth,                                      X
                PSECT=RACFAUT1
 
-sear_racroute_auth_asm ALIAS C'sear_racroute_auth_asm'
+callRauth ALIAS C'sear_racroute_auth_asm'
          USING AUTOSTG,R4
          USING MYPARMS,R11
 
@@ -110,7 +130,7 @@ RACFPLCL EQU   *-RACFPLC
 RACFPLA  RACROUTE REQUEST=AUTH,ATTR=ALTER,CLASS='DUMMY',               X
                RELEASE=2.4,MF=L
 RACFPLAL EQU   *-RACFPLA
-RACFPLTL EQU   RACFPLAL
+RACFPLTL EQU   RACFPLRL
 
 AUTOSTG  DSECT ,
          CEEDSA SECTYPE=XPLINK
